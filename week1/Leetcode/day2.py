8bb2ca379e38e24626d81ea12bb88056d32f5fa7
Veer <viraj1panwar@gmail.com>
2026-01-29 20:25:10 +0530

    added leetcode folder along with day 2 solutions

---
+++ week1/Leetcode/day2.py
@@ -0,0 +1,25 @@
+# LeetCode 1: Two Sum (save as function)
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[num] = i
+
+print(twoSum([2,7,11,15], 9))
+
+# LeetCode 2: FizzBuzz
+def fizzBuzz(n):
+    result = []
+    for i in range(1, n+1):
+        if i % 15 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    return result
+print(fizzBuzz(20))
